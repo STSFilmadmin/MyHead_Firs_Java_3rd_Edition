b23a40c415ab61cfa7f04fe30d555f996ef433e8
Vasyl.Boilo <vasyl.boilo@gmail.com>
2023-04-03 21:26:50 +0300

    Умовне розгалуження (if)

---
+++ HeadFirstJava3rdEdotion/src/start/IfTest.java
@@ -0,0 +1,11 @@
+package start;
+
+class IfTest {
+	  public static void main (String[] args) {
+	    int x = 3;
+	    if (x == 3) {
+	      System.out.println("x must be 3/ х має бути 3");
+	    }
+	    System.out.println("This runs no matter what/Це працює незважаючи ні на що");
+	  }
+	}
