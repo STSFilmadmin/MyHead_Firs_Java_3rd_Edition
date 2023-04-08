352794784fc9f1e84d4c358f463846d87b25ecc6
Vasyl.Boilo <vasyl.boilo@gmail.com>
2023-04-08 21:47:09 +0300

    chapter2 гра вгадайка змінено закінчення (приведено вивід до кращого вигляду)

--- HeadFirstJava3rdEdotion/src/chapter2/GuessGame.java
+++ HeadFirstJava3rdEdotion/src/chapter2/GuessGame.java
@@ -6,59 +6,70 @@ public class GuessGame {
 	Player p3;
 
 	public void startGame() {
-		p1= new Player();
-		p2= new Player();
-		p3= new Player();
-		
-		int guessp1 =0;
-		int guessp2 =0;
-		int guessp3 =0;
-		
-		boolean p1isRigth = false;
-		boolean p2isRigth = false;
-		boolean p3isRigth = false;
-		
+		p1 = new Player();
+		p2 = new Player();
+		p3 = new Player();
+
+		int guessp1 = 0;
+		int guessp2 = 0;
+		int guessp3 = 0;
+
+		boolean p1isRight = false;
+		boolean p2isRight = false;
+		boolean p3isRight = false;
+
 		int targetNumber = (int) (Math.random() * 10);
-	    System.out.println("Я думаю про число від 0 до 9...");
+		System.out.println("Я думаю про число від 0 до 9...");
 
-	    while (true) {
-	        System.out.println("Число для вгадування " + targetNumber);
-	        p1.guess();
-	        p2.guess();
-	        p3.guess();
+		while (true) {
+			System.out.println("Число для вгадування " + targetNumber);
+			p1.guess();
+			p2.guess();
+			p3.guess();
 
-	        guessp1 =  p1.number;
-	        System.out.println("Перший гравець загадав " + guessp1);
-	        
-	        guessp2 =  p2.number;
-	        System.out.println("Другий гравець загадав " + guessp2);
-	        
-	        guessp3 =  p3.number;
-	        System.out.println("Третій гравець загадав " + guessp3);
-	        
-	       if (guessp1 == targetNumber) {
-	    	   p1isRigth = true;
-	       }
-	       if (guessp2 == targetNumber) {
-		    	   p2isRigth = true;   
-	       }	   
-		   if (guessp3 == targetNumber) {
-			    	   p3isRigth = true;
-			    	   
-			if (p1isRigth || p2isRigth || p3isRigth) {
+			guessp1 = p1.number;
+			System.out.println("Перший гравець загадав " + guessp1);
+
+			guessp2 = p2.number;
+			System.out.println("Другий гравець загадав " + guessp2);
+
+			guessp3 = p3.number;
+			System.out.println("Третій гравець загадав " + guessp3);
+
+			if (guessp1 == targetNumber) {
+				p1isRight = true;
+			}
+			if (guessp2 == targetNumber) {
+				p2isRight = true;
+			}
+			if (guessp3 == targetNumber) {
+				p3isRight = true;
+			}
+			if (p1isRight || p2isRight || p3isRight) {
 				System.out.println("У нас є переможець ");
-				System.err.println("Грвець під номером один вгадав? "+ p1isRigth);
-				System.err.println("Грвець під номером два вгадав? "+ p2isRigth);
-				System.err.println("Грвець під номером Три вгадав? "+ p3isRigth);
+				if (p1isRight == true) {
+					System.err.println("Гравець під номером один вгадав! ");
+				} else {
+					System.err.println("Гравець під номером один не вгадав! ");
+				}
+				if (p2isRight == true) {
+					System.err.println("Гравець під номером два вгадав! ");
+				} else {
+					System.err.println("Гравець під номером два не вгадав! ");
+				}
+				if (p3isRight == true) {
+					System.err.println("Гравець під номером Три вгадав!");
+				} else {
+					System.err.println("Гравець під номером три не вгадав! ");
+
+				}
+
 				System.out.println("Game Over");
 				break;
-			}else {
+			} else {
 				System.out.println("Ніхто з тих тупауів не вгадав прийдеться грати знову....");
-			}    	   
-			    	   
-			    	   
-		   }   
-	       }
-	       }
-		
+			}
+
+		}
 	}
+}
